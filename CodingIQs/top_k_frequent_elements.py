b60420ebf2f74c3531092b2590e58bfa690d871c
Aditya Kar <61394999+akar016012@users.noreply.github.com>
2023-04-12 21:21:00 -0500

    added top_k_frequent_elements.py

---
+++ CodingIQs/top_k_frequent_elements.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        hashmap = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            hashmap[n] = 1 + hashmap.get(n, 0)
+        for n, c in hashmap.items():
+            freq[c].append(n)
+
+        res = []
+
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
