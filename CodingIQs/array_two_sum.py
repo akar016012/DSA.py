d32f76ab3047cf4bda38fd011fe4c2cd97033447
Aditya Kar <61394999+akar016012@users.noreply.github.com>
2023-05-10 22:13:26 -0500

    added three sum

--- CodingIQs/array_two_sum.py
+++ CodingIQs/array_two_sum.py
@@ -31,3 +31,37 @@ def twoSum(self, numbers, target):
             l += 1
         else:
             r -= 1
+
+
+# My Documentation:
+"""
+# Approach
+Two-pointer approach
+
+# Complexity
+- Time complexity:
+Since, we are looping through the list once, the time complexity would be $$O(n)$$
+
+- Space complexity:
+Since, we are not using any extra space, the space complexity would be $$O(1)$$
+
+# Code
+```
+class Solution(object):
+    def twoSum(self, numbers, target):
+        l, r = 0, len(numbers)-1
+        while l < r:
+            #--Adding the numbers to get the total sum
+            numSum = numbers[l] + numbers[r]
+            #--If sum is less than the target move the left point up 
+            if numSum < target:
+                l+=1
+            #--If sum is less than the target move the right point down 
+            elif numSum > target:
+                r-=1
+            # If the sum equals the target return the two indexes
+            else:
+                return [l+1,r+1]
+                    
+```
+"""
